0cb3205d21c4133463673c52fb7052a332afbe50
Arktiks <arktiks.email@gmail.com>
2016-10-11 21:59:28 +0300

    Small changes to Players.h

---
+++ Players.h
@@ -0,0 +1,15 @@
+#include "../Logging/Memory.h"
+#include "../GameManager.h"
+
+#include "Monkey.h"
+
+GameManager SetGame()
+{
+    GameManager manager;
+    manager.AddPlayer(NEW Monkey("Loiri"));
+    manager.AddPlayer(NEW Monkey("Mielikki"));
+    manager.AddPlayer(NEW Monkey("Koiro"));
+    manager.AddPlayer(NEW Monkey("Hessu Hopo"));
+    manager.AddPlayer(NEW Monkey("Aurinko"));
+    return manager;
+}
